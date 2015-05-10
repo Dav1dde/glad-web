864b15b40a40dc6bfb737b35d47e2dcb115a9578
Dav1dde <admin@dav1d.de>
2015-05-10 12:43:18 +0200

    app: add a way to enable logging via commandline create_app

--- gladweb/__init__.py
+++ gladweb/__init__.py
@@ -1,14 +1,62 @@
 from flask import Flask, g
 from gladweb.metadata import Metadata
 import gladweb.util
+import logging
+import sys
 
 
-def create_application():
+class LevelFilter(logging.Filter):
+    def __init__(self, levels):
+        self.levels = levels
+
+    def filter(self, rec):
+        return rec.levelno in self.levels
+
+
+def setup_logging():
+    root_logger = logging.getLogger()
+    root_logger.handlers = []
+
+    fmtr = logging.Formatter(
+        '[%(asctime)s] %(levelname)s:\t%(message)s', '%m/%d/%Y %H:%M:%S'
+    )
+
+    stdout = logging.StreamHandler(sys.stdout)
+    low_filter = LevelFilter((logging.DEBUG, logging.INFO))
+    stdout.addFilter(low_filter)
+    stdout.setFormatter(fmtr)
+    root_logger.addHandler(stdout)
+
+    stderr = logging.StreamHandler(sys.stderr)
+    high_filter = LevelFilter(
+        (logging.WARNING, logging.ERROR, logging.CRITICAL)
+    )
+    stderr.addFilter(high_filter)
+    stderr.setFormatter(fmtr)
+    root_logger.addHandler(stderr)
+
+    root_logger.setLevel(logging.DEBUG)
+
+
+def create_application(debug=False, verbose=False):
+    """
+    Creates fully configured flask application
+
+    :param debug: Launch the app in debug-mode.
+    :param verbose: True enables logging,
+                    None (default) only enables logging if not in debug mode
+                    False no logging configured.
+    :return: The created Flask application
+    """
     import gladweb.config
 
     app = Flask(__name__)
     app.config.from_object('gladweb.config')
     app.config['METADATA'] = Metadata(app.config['CACHE'])
+    app.debug = debug
+
+    if verbose or (not app.debug and verbose is None):
+        setup_logging()
 
     @app.before_request
     def before_request():
--- gladweb/__main__.py
+++ gladweb/__main__.py
@@ -7,12 +7,11 @@ def main():
     parser.add_argument('--port', default=5000, help='Port to listen on.')
     parser.add_argument('--host', default='127.0.0.1', help='Host to bind to.')
     parser.add_argument('--debug', action='store_true', help='TESTING ONLY')
+    parser.add_argument('--verbose', action='store_true', help='Log to stdout and stderr')
     ns = parser.parse_args()
 
-    app = create_application()
-    app.run(
-        debug=ns.debug, port=int(ns.port), host=ns.host
-    )
+    app = create_application(debug=ns.debug, verbose=ns.verbose)
+    app.run(port=int(ns.port), host=ns.host)
 
 
 if __name__ == '__main__':
