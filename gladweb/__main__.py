d1d99c3fb06471086177b387f4cb815b00c72c5c
Dav1dde <admin@dav1d.de>
2015-07-29 17:52:57 +0200

    glad: Added cron subcommand.

--- gladweb/__main__.py
+++ gladweb/__main__.py
@@ -1,22 +1,51 @@
 import argparse
+import shutil
 import sys
+import time
 from gladweb import create_application
+import os
 
 
 def init(ns):
     import gladweb.config
+
+    _refresh_metadata(gladweb.config.CACHE)
+
+
+def _refresh_metadata(cache, diff=60):
     from gladweb.metadata import Metadata
 
-    cache = gladweb.config.CACHE
-    cache.clear()
-
-    # refreshes the metadata, since the cache is empty
-    Metadata(cache)
+    m = Metadata(cache)
+    if time.time() - m.created > diff:
+        cache.clear()
+        m.refresh_metadata()
 
 
 def www(ns):
     app = create_application(debug=ns.debug, verbose=ns.verbose)
     app.run(port=int(ns.port), host=ns.host)
+
+
+def cron(ns):
+    import gladweb.config
+
+    diff = ns.age * 60 * 60 if ns.age >= 0 else 0
+    _refresh_metadata(gladweb.config.CACHE, diff)
+
+    if ns.age < 0:
+        return
+
+    temp = gladweb.config.TEMP
+    for name in os.listdir(temp):
+        path = os.path.join(temp, name)
+        mtime = os.stat(path).st_mtime
+        age = time.time() - mtime
+        hours = age / 60 / 60
+        if hours > ns.age:
+            if os.path.isfile(path) or os.path.islink(path):
+                os.remove(path)
+            else:
+                shutil.rmtree(path)
 
 
 def main():
@@ -30,6 +59,10 @@ def main():
     www_parser.add_argument('--host', default='127.0.0.1', help='Host to bind to.')
     www_parser.add_argument('--debug', action='store_true', help='TESTING ONLY')
     www_parser.add_argument('--verbose', action='store_true', help='Log to stdout and stderr')
+
+    cron_parser = subparsers.add_parser('cron')
+    cron_parser.add_argument('--age', type=int, default=24,
+                             help='Temporary files will be deleted after X hours')
 
     ns = parser.parse_args()
 
