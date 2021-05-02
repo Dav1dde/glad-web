6697a5102077578f4259643e70791584a18c5e8e
David Herberth <admin@dav1d.de>
2021-05-02 11:04:26 +0200

    docker: reduces workers to 5

--- docker/gunicorn.config.py
+++ docker/gunicorn.config.py
@@ -3,7 +3,7 @@ import os.path
 base_path = os.path.split(os.path.abspath(__file__))[0]
 
 bind = '0.0.0.0:8080'
-workers = 9
+workers = 5
 worker_class = 'eventlet'
 keepalive = 5
 loglevel = 'warning'
